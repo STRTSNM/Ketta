4d8f9470c3d968a2bdface9eb0c37c0ac67986d8
strtsnm-tamil <64964414+STRTSNM@users.noreply.github.com>
2021-07-04 21:49:38 +0530

    Add files via upload

--- pec_audio.py
+++ pec_audio.py
@@ -14,31 +14,33 @@ with data as source:
     print(text)
 
 if "Alexa" in text:
-    if "open" in text:
-        if "Chrome" in text:
-            os.system("google-chrome-stable")
-    if "play" in text:
-            print("play in text")
-            driver = webdriver.Firefox(executable_path="/home/garuda/Github projects/Virtual-assistant/geckodriver")
-            driver.implicitly_wait(5)
+    text.replace("Alexa", "")
 
-            # Let's create a list of terms to search for and an empty list for links
-            search_terms = [text]
-            links = []
+if "open" in text:
+    if "Chrome" in text:
+        os.system("python open.py chrome")
+if "play" in text:
+        print("play in text")
+        driver = webdriver.Firefox(executable_path="/home/garuda/Github projects/Virtual-assistant/geckodriver")
+        driver.implicitly_wait(5)
 
-            for term in search_terms:
-                # Open YouTube page for each search term
-                driver.get('https://www.youtube.com/results?search_query={}'.format(term))
-                # Find a first webelement with video thumbnail on the page
-                link_webelement = driver.find_element(By.CSS_SELECTOR, 'div#contents ytd-item-section-renderer>div#contents a#thumbnail')
-                # Grab webelement's href, this is your link, and store in a list of links:
-                links += [link_webelement.get_attribute('href')]
-    else:
-        print("no useful commands found")    
+        # Let's create a list of terms to search for and an empty list for links
+        search_terms = [text]
+        links = []
+
+        for term in search_terms:
+            # Open YouTube page for each search term
+            driver.get('https://www.youtube.com/results?search_query={}'.format(term))
+            # Find a first webelement with video thumbnail on the page
+            link_webelement = driver.find_element(By.CSS_SELECTOR, 'div#contents ytd-item-section-renderer>div#contents a#thumbnail')
+            # Grab webelement's href, this is your link, and store in a list of links:
+            links += [link_webelement.get_attribute('href')]
+else:
+    print("no useful commands found")    
 
 
-    print(links)
-    os.system("google-chrome-stable "+links[0])
-    driver.quit()
-    
+print(links)
+driver.quit()
+os.system("google-chrome-stable "+links[0])
+
 print("2 done")
